6ebc187f0028eb82f65d8a60cf88ef1a0fdf99ed
Cidar de Alencar <101343945+CidardeAlencar@users.noreply.github.com>
2023-12-16 11:42:46 -0400

    pagina en construccion

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,22 +3,27 @@ function FooterComponent() {
     <div className="container py-1">
         <div className="row">
             <div className="col-12 col-md-6 text-light text-center font-class">
-            Todos los derechos reservados GOPHORA INC.
+                {/* Todos los derechos reservados  */}
+                HECHO CON <i className="bi bi-heart-fill text-danger"></i> EN  LA PAZ, BO | FLORIDA USA
             </div>
-            <div className="col-12 col-md-6 d-flex justify-content-evenly align-items-center">
+            <div className="col-12 col-md-3 text-light text-center font-class">
+                {/* Todos los derechos reservados  */}
+                GOPHORA INC.
+            </div>
+            <div className="col-12 col-md-3 d-flex justify-content-evenly align-items-center">
                 <a href="https://www.instagram.com/gophoranftcollectibles/" target="_blank" rel="noopener noreferrer">
                     <i className="bi bi-instagram"></i>
                 </a>
                 <a href="https://twitter.com/GophoraNft" target="_blank" rel="noopener noreferrer">
                     <i className="bi bi-twitter-x"></i>
                 </a>
-                <a href="https://discord.gg/jbhzwjfcpV" target="_blank" rel="noopener noreferrer">
+                {/* <a href="https://discord.gg/jbhzwjfcpV" target="_blank" rel="noopener noreferrer">
                     <i className="bi bi-discord"></i>
-                </a>
+                </a> */}
                 <a href="https://wa.me/59170516511" target="_blank" rel="noopener noreferrer">
                     <i className="bi bi-whatsapp"></i>
                 </a>
-                <a href="https://www.youtube.com/watch?v=EtGktRtrBRw" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.youtube.com/@andreavcovarrubias9264" target="_blank" rel="noopener noreferrer">
                     <i className="bi bi-youtube"></i>
                 </a>
 
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
-import backgroundImage from "../assets/planetaBackground.png";
-import criptopurrs from "../assets/criptopurrs1.png";
-import luna from "../assets/NFT LUNA.png";
+// import { Link } from 'react-router-dom';
+// import backgroundImage from "../assets/planetaBackground.png";
+// import criptopurrs from "../assets/criptopurrs1.png";
+// import luna from "../assets/NFT LUNA.png";
 // import tierra from "../assets/NFT TIERRA.png";
-import marte from "../assets/NFT MARTE.png";
+// import marte from "../assets/NFT MARTE.png";
+// import team from "../assets/team1.png";
 function HomeScreen() {
   return (
-    <section className="home-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    // <section className="home-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <section className="home-section">
       <div className="container-fluid d-flex flex-column justify-content-center text-center p-0">
         <div className="line-custom py-1">
           EXPANDIMOS LOS HORIZONTES DE LO POSIBLE, Conectamos toda voluntad con la Exploración Espacial
@@ -14,67 +16,9 @@ function HomeScreen() {
         <div className="line-custom2 py-1">
           Horizontes Personalizados de Experiencia, Trabajo y Educación Potenciados por I.A. + Evidencia de Habilidad Excepcional sobre Blockchain 
         </div>
-        {/* body */}
-        <div className="container">
-          <div className="row d-flex justify-content-center align-items-center">
-            <div className="col-12 col-xl-6">
-                {/* <div>
-                    <a href="https://lanzatunombrealespacio.gophora.com/" target="_blank" rel="noopener noreferrer" className="row text-primary justify-content-center hashtag pt-2">
-                      #gophoralanzatunombrealespacio
-                    </a>
-                </div>   */}
-
-                <button>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLScgc5Gtzrs3qHjWa4qDgfyAQtabUO1jBVG6IGq0qPWroXACmA/viewform" target="_blank" rel="noopener noreferrer" className=" text-primary justify-content-center hashtag pt-2">
-                    ¡Quiero ser parte de GOPHORA STARSHIP!
-                    </a>
-                </button>          
-                <div className="row">
-                  {/* <div className="col-6 p-0 d-flex justify-content-end">
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfiOc65tu-f_UIUUa00lV8cz3CouQ7gRVeaWh_R52McyPvgXw/viewform" target='_blank' rel='nooppener noreferrer'>
-                      <img src={luna} alt="Plan luna" className="img-fluid img-button"/>
-                    </a>
-                  </div> */}
-                  {/* <div className="col-4 p-0">
-                    <img src={tierra} alt="Plan tierra" className="img-fluid"/>
-                  </div> */}
-                  <div className="col-6 p-0 d-flex justify-content-start">
-                    {/* <Link to="/comprar">
-                      <img src={marte} alt="Plan marte" className="img-fluid img-button"/>
-                    </Link> */}
-                  </div>
-                </div>
-                <div className="row mb-1">
-                  {/* <div className="col-lg-1 col-md-0"></div> */}
-                  {/* <div className="col-lg-4 col-md-12 bitacora-custom py-2 px-0 d-flex flex-column align-items-center justify-content-center">
-                    <div>
-                    <span className="text-warning">BITACORA DE VIAJE_</span><br/>
-                    HORIZONTE DE EXPLORACIÓN, 
-                    ESTRATOSFERA.
-                    <br/><span className="text-warning">LANZAMIENTO_</span><br/>
-                    11/11/23
-                    ESTACIÓN TERRESTRE ABE AMACHUMA.
-                    <br/><span className="text-warning">TRIPULACIÓN_ </span> 
-                    190.
-                    </div>
-                  </div> */}
-                  {/* <div className="col-lg-4 col-md-12 d-flex align-items-center justify-content-center px-0">
-                    <Link to="/comprar">
-                      <button type="button" className="btn btn-outline-warning button-custom-marte py-xxl-4 py-xl-4 py-lg-5">Comprar <br/>MARTE ID NFT</button>
-                    </Link>
-                  </div> */}
-                  {/* <div className="col-lg-4 col-md-12 d-flex align-items-center justify-content-center justify-content-lg-start px-0">
-                    <Link to="/mintear">
-                      <button type="button" className="btn btn-outline-warning button-custom-marte btn-custom py-xxl-5 py-xl-5 py-lg-5">MINTEAR<br/>NFT</button>
-                    </Link>
-                  </div> */}
-                </div>
-            </div>
-            <div style={{paddingTop:"100px"}} className="col-12 col-xl-6 d-flex justify-content-center align-items-center">
-              {/* <img src={criptopurrs} alt="Criptopurrs" className="img-full-height"/> */}
-              <iframe  width="560" height="315" src="https://www.youtube.com/embed/naBS-GcqSs8?si=CnG4XG-exXW8X62M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-          </div>
+        <div className="under-construction-message line-custom3">
+          <h1>Volvemos Pronto</h1>
+          <p>Página en Construcción</p>
         </div>
       </div>
     </section>
