f71845c8b98d1f9318128705ee310903e373a696
Cidar de Alencar <101343945+CidardeAlencar@users.noreply.github.com>
2024-01-14 17:18:03 -0400

    frontV3 update team screen

--- src/screens/TeamScreen.jsx
+++ src/screens/TeamScreen.jsx
@@ -6,10 +6,24 @@ import backgroundImage from "../assets/ABE.png";
 // import nft5 from "../assets/5.png";
 function TeamScreen() {
   return (
-    <section className="home-section font-class d-flex flex-column justify-content-end" style={{ backgroundImage: `url(${backgroundImage})`,minHeight: "100vh" }}>
+    // <section className="home-section font-class d-flex flex-column justify-content-end" style={{ backgroundImage: `url(${backgroundImage})`,minHeight: "100vh" }}>
+    <section className="font-class d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-warning mb-5 desplegar">TEAM</h1>
+        
         <div className="container my-2">
+            <img src={backgroundImage} alt="team" className="img-fluid" />
             {/* <h1 className="text-warning mb-5">TEAM</h1> */}
-            <div className="row row-cols-1  row-cols-sm-2 row-cols-md-5 g-3">
+            <h3 className="text-warning p-5">Founders</h3>
+            <div className="row row-cols-1  row-cols-sm-2 row-cols-md-4 g-3">
+                <div className="col">
+                    <div className="card h-100 card-background">
+                        {/* <img src={nft2} className="card-img-top" alt="VISNITY_"/> */}
+                        <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
+                            <h5 className="card-title title-font" style={{ color: '#B377FF' }}>VISNITY_</h5>
+                            <p className="card-text text-font">FOUNDER<br/> LEADER<br/>VISIONARY<br/>STRATEGIC PLANNER<br/>COMUNICATOR<br/> CEO</p>
+                        </div>
+                    </div>
+                </div>
                 <div className="col">
                     <div className="card h-100 card-background">
                         {/* <img src={nft1} className="card-img-top" alt="CARBONFIRE_"/> */}
@@ -21,18 +35,9 @@ function TeamScreen() {
                 </div>
                 <div className="col">
                     <div className="card h-100 card-background">
-                        {/* <img src={nft2} className="card-img-top" alt="VISNITY_"/> */}
-                        <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
-                            <h5 className="card-title title-font">VISNITY_</h5>
-                            <p className="card-text text-font">FOUNDER<br/> LEADER<br/>VISIONARY<br/>STRATEGIC PLANNER<br/>COMUNICATOR<br/> CEO</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="card h-100 card-background">
                     {/* <img src={nft4} className="card-img-top" alt="DEALENCAR_"/> */}
                     <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
-                        <h5 className="card-title title-font">DE ALENCAR_</h5>
+                        <h5 className="card-title title-font" style={{ color: '#438BE6' }}>ALENCAR_</h5>
                         <p className="card-text text-font">COFOUNDER <br/>WEB, MOBILE<br/> FRONTEND<br/> AR / VR<br/> METAVERSE DEV.<br/>CIO</p>
                     </div>
                     </div>
@@ -41,20 +46,20 @@ function TeamScreen() {
                     <div className="card h-100 card-background">
                     {/* <img src={nft3} className="card-img-top" alt="TOMMATOMIC_"/> */}
                     <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
-                        <h5 className="card-title title-font">TOMMATOMIC_</h5>
+                        <h5 className="card-title title-font" style={{ color: '#C9F77F' }}>TOMATOMIC_</h5>
                         <p className="card-text text-font">COLABORATOR <br/>FRONTEND<br/> LOGISTICS <br/>BLOCKCHAIN<br/>COO</p>
                     </div>
                     </div>
                 </div>
-                <div className="col">
+                {/* <div className="col">
                     <div className="card h-100 card-background">
-                    {/* <img src={nft5} className="card-img-top" alt="DEALENCAR_"/> */}
+                    <img src={nft5} className="card-img-top" alt="DEALENCAR_"/>
                     <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
                         <h5 className="card-title title-font">OSCAR VC_</h5>
                         <p className="card-text text-font">INVESTOR<br/>COLABORATOR<br/>MARKETING<br/>SALES<br/>DIGITAL<br/>CHANNELS<br/>CMO</p>
                     </div>
                     </div>
-                </div>
+                </div> */}
             </div>
             {/* <div className="row">
                 <div className="dropdown-center d-flex justify-content-center align-items-center m-3">
@@ -68,6 +73,37 @@ function TeamScreen() {
                     </ul>
                 </div>
             </div> */}
+            <h3 className="text-warning p-5">Colaboradores</h3>
+            <div className="row row-cols-1  row-cols-sm-2 row-cols-md-3 g-3">
+                <div className="col">
+                    <div className="card h-100 card-background">
+                        {/* <img src={nft2} className="card-img-top" alt="VISNITY_"/> */}
+                        <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
+                            <h5 className="card-title title-font" style={{ color: '#FF8617' }}>PERRY_</h5>
+                            <p className="card-text text-font">COMUNIDADES<br/> UNIVERSITARIAS<br/>EVENTOS<br/>PATROCINIOS</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100 card-background">
+                        {/* <img src={nft1} className="card-img-top" alt="CARBONFIRE_"/> */}
+                        <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
+                            <h5 className="card-title title-font" style={{ color: '#E38EB2' }}>SERGIO_</h5>
+                            <p className="card-text text-font">NETWORK VC<br/>EXPERIENCIAS GS<br/> VENTAS Y MERCADO<br/> IMPLEMENTACIÓN</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col">
+                    <div className="card h-100 card-background">
+                    {/* <img src={nft4} className="card-img-top" alt="DEALENCAR_"/> */}
+                    <div className="card-body d-flex flex-column justify-content-start align-items-center my-4">
+                        <h3 className="card-title title-font text-warning">Ser Colaborador:</h3>
+                        <p className="card-text text-font">Envia tu Hoja de Habilidades</p>
+                        <h5 className="card-title title-font text-warning">nftcollectibles@gophora.com</h5>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
   )
